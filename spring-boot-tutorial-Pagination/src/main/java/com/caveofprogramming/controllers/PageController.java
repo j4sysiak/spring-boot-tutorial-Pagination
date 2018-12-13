159611a8c4ea81170fdab028b12d10a3788aec7e
j4sysiak <j4sysiak@gmail.com>
2018-12-13 06:07:19 +0100

    22

--- spring-boot-tutorial-Pagination/src/main/java/com/caveofprogramming/controllers/PageController.java
+++ spring-boot-tutorial-Pagination/src/main/java/com/caveofprogramming/controllers/PageController.java
@@ -8,6 +8,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.caveofprogramming.model.StatusUpdate;
@@ -28,8 +29,19 @@ public class PageController {
 	String about() {
 		return "app.about";
 	}
+
+	@RequestMapping(value = "/viewstatus", method = RequestMethod.GET)
+	ModelAndView viewStatus(ModelAndView modelAndView, @RequestParam(name="p", defaultValue="1") int pageNumber) {
+		
+		System.out.println();
+		System.out.println("=========" + pageNumber + "=========");
+		System.out.println();
+		
+		modelAndView.setViewName("app.viewStatus");
+
+		return modelAndView;
+	}
 	
-	 
 	@RequestMapping(value = "/addstatus", method = RequestMethod.GET)
 	ModelAndView addStatus(ModelAndView modelAndView, @ModelAttribute("statusUpdate") StatusUpdate statusUpdate) {
 
