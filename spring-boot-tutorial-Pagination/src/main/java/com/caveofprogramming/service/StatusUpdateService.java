e77507d3e58f109be2e95d267d5fef5f3af837f5
j4sysiak <j4sysiak@gmail.com>
2018-12-14 05:25:53 +0100

    11

--- spring-boot-tutorial-Pagination/src/main/java/com/caveofprogramming/service/StatusUpdateService.java
+++ spring-boot-tutorial-Pagination/src/main/java/com/caveofprogramming/service/StatusUpdateService.java
@@ -1,6 +1,9 @@
 package com.caveofprogramming.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.caveofprogramming.model.StatusUpdate;
@@ -8,6 +11,8 @@ import com.caveofprogramming.model.StatusUpdateDao;
 
 @Service
 public class StatusUpdateService {
+	
+	private final static int PAGESIZE = 3;
 	
 	@Autowired
 	private StatusUpdateDao statusUpdateDao;
@@ -18,5 +23,11 @@ public class StatusUpdateService {
 	
 	public StatusUpdate getLatest() {
 		return statusUpdateDao.findFirstByOrderByAddedDesc();
+	}
+	
+	public Page<StatusUpdate> getPage(int pageNumber) {
+		PageRequest request = PageRequest.of(pageNumber-1, PAGESIZE, Sort.Direction.DESC, "added");
+		
+		return statusUpdateDao.findAll(request);
 	}
 }
